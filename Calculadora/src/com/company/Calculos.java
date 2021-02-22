e18648006124837878073f3c30cc580b4b020185
frankloo3 <78281249+frankloo3@users.noreply.github.com>
2021-02-21 21:28:47 -0500

    Add files via upload

---
+++ Calculadora/src/com/company/Calculos.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Calculos {
+    //Atributos
+    public int num1;
+    public int num2;
+
+    //Constructor
+    public Calculos(){
+    }
+    public Calculos (int num1, int num2){
+        this.num1 = num1;
+        this.num2 = num2;
+    }
+
+    public int sumar(){
+        int suma = this.num1 = this.num2;
+        return suma;
+    }
+
+}
---
+++ Calculadora/src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Calculos calculadora = new Calculos (102, 52);
+        int suma = calculadora.sumar();
+        System.out.println("La suma de los numeros es: "+ suma);
+
+
+        }
+
+}
